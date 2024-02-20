888e400e780354196c9ed24fb13d5283c9003ca8
jph079 <jph079@educa.madrid.org>
2024-02-20 10:40:48 +0100

    mergerama1

--- programa2.java
+++ programa2.java
@@ -3,10 +3,11 @@ package com.iesinfantaelena;
 public class programa2 {
 
 	public static void main(String[] args) { System.out.println("1er commit rama2");System.out.println("2o commit rama2");
-		// TODO Auto-generated method stub
-		System.out.println("1er commit conjunto");
-		System.out.println("2o commit conjunto");
-		System.out.println("3er commit conjunto");
+	// TODO Auto-generated method stub
+	System.out.println("1er commit rama1");
+	System.out.println("2o commit rama1");
+	System.out.println("3er commit rama1");
+
 	}
 
 }
--- programa3.java
+++ programa3.java
@@ -4,9 +4,11 @@ public class programa3 {
 
 	public static void main(String[] args) { System.out.println("1er commit rama2");System.out.println("2o commit rama2");
 		// TODO Auto-generated method stub
-		System.out.println("1er commit conjunto");
-		System.out.println("2o commit conjunto");
-		System.out.println("3er commit conjunto");
+		System.out.println("1er commit rama1");
+		System.out.println("2o commit rama1");
+		System.out.println("3er commit rama1");
+		
+	
 	}
 
 }
