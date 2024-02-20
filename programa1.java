3f25a89062bd88b00c8a6927346a76103151a2b7
jph079 <jph079@educa.madrid.org>
2024-02-20 10:33:31 +0100

    capturas rama1

--- programa1.java
+++ programa1.java
@@ -4,13 +4,11 @@ public class programa1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("1er commit conjunto");
-		System.out.println("2o commit conjunto");
-		System.out.println("3er commit conjunto");
-		
 		System.out.println("1er commit rama1");
 		System.out.println("2o commit rama1");
 		System.out.println("3er commit rama1");
+		
+	
 	}
 
 }
