1e2f48db1b91eb2589cbeb6569c3940fd04d1cc6
jph079 <jph079@educa.madrid.org>
2024-02-20 10:42:26 +0100

    ultimocambiorama1

--- programa2.java
+++ programa2.java
@@ -3,10 +3,13 @@ package com.iesinfantaelena;
 public class programa2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		for (int i = 0; i < args.length; i++) {
+			System.out.println("prueba");
+		}// TODO Auto-generated method stub
 		System.out.println("1er commit rama1");
 		System.out.println("2o commit rama1");
 		System.out.println("3er commit rama1");
+		
 		
 
 	}
